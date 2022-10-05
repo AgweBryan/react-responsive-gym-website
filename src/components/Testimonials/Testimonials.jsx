75b4ebcae73f32a3ef2ff6f31d9f02f4f1190b64
AgweBryan <agwebryanmunakwa@gmail.com>
2022-10-06 00:39:27 +0100

    complete testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style/testimonials.css";
+import { testimonialsData } from "../../data/testimonialsData";
+import rightArrow from "../../assets/rightArrow.png";
+import leftArrow from "../../assets/leftArrow.png";
 
 export const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const [selected, setSelected] = useState(0);
+  const tLength = testimonialsData.length;
+
+  return (
+    <div className="testimonials">
+      <div className="testimonials__left">
+        <span>Testimonials</span>
+        <span className="stroke-text">What they</span>
+        <span>say about us</span>
+        <span>{testimonialsData[selected].review}</span>
+        <span>
+          <span
+            style={{
+              color: "var(--orange)",
+            }}
+          >
+            {testimonialsData[selected].name}
+          </span>{" "}
+          ~ {testimonialsData[selected].status}
+        </span>
+      </div>
+      <div className="testimonials__right">
+        <div></div>
+        <div></div>
+        <img src={testimonialsData[selected].image} alt="testimonials image" />
+        <div className="testimonials__right--arrows">
+          <img
+            onClick={() =>
+              selected == 0
+                ? setSelected(tLength - 1)
+                : setSelected((prev) => prev - 1)
+            }
+            src={leftArrow}
+            alt="left arrow"
+          />
+          <img
+            onClick={() =>
+              selected == tLength - 1
+                ? setSelected(0)
+                : setSelected((prev) => prev + 1)
+            }
+            src={rightArrow}
+            alt="right arrow"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
